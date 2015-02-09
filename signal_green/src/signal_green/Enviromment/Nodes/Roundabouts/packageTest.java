961754483f265c1b7fc7ea9e006bf52a4f7ca05f
JamesWKerr <james.william.kerr@gmail.com>
2015-02-09 03:37:51 +0000

    morePackageLayout

---
+++ signal_green/src/signal_green/Enviromment/Nodes/Roundabouts/packageTest.java
@@ -0,0 +1,17 @@
+
+//if memory servves me correctly, directories and java packages are eqiulvant?
+
+package architecture;
+public class packageTest{
+
+public static void Main(String[] args){
+
+System.out.println("are packages this easy?");
+
+return;
+	} 
+}
+
+
+
+

36de778025654ef15393ef193fb51efc832c7872
James William Kerr <james.william.kerr@gmail.com>
2015-02-12 04:27:25 +0000

    my role in this

---
+++ signalGreen/src/signalGreen/agentplan.java
@@ -0,0 +1,23 @@
+
+
+/* Because simphony only seems to care about nodes not edges, my idea for implemnting proper,
+decentralised BDI(more advanced form of agents) is to have something along these lines */
+
+
+class MainEdge extends a proper repast edge I think.
+	Node source
+	Node target
+	float weight
+	int lanes(ambitiuous...)
+
+	from this, we devide to the edge into subsections, so that a car can only see a section ahead of it.
+	Queue<SubEdges>(10) //can divide length from constructor to get exact amount,
+
+Instead of using the watcher, which is a centralised view and not really agents
+this allows each agent to percieve the enviroment itself, and act accordingly, to its beliefs.
+
+modularilised well enough as an interface perhaps this could work with most things.
+
+
+I-ll start on the code tomo
+

0fb159cc410297c2c2f758f4c84882732e0a6b42
waqaraaziz <waqar.aziz@kcl.ac.uk>
2015-02-18 05:38:16 +0000

    Added multilane implementation with comments
    Junction can now have any number of user-specified lanes between other junctions. This eliminates the limitations from the earlier implementation. Each method has been commented with explanation.

--- signalGreen/src/signalGreen/Junction.java
+++ signalGreen/src/signalGreen/Junction.java
@@ -1,69 +1,144 @@
 package signalGreen;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import repast.simphony.space.continuous.ContinuousSpace;
 import repast.simphony.space.continuous.NdPoint;
 import repast.simphony.space.graph.Network;
+import repast.simphony.space.graph.RepastEdge;
 import repast.simphony.space.grid.Grid;
 
+/**
+ * Generic class for junctions of the Traffic Simulator.<br />
+ * 
+ * A Junction object is a node on a Graph. It can have a lane to
+ * any number of other Junction objects. Each lane can have 
+ * the direction specified and these lanes represent Edges 
+ * on the Graph.
+ * 
+ * TrafficLights is a subclass of Junction, and has the special 
+ * behavior of scheduling traffic light management.
+ * 
+ * @author Waqar
+ *
+ */
 public class Junction {
 
+	/**
+	 * Prints out number of Junctions that this Junction has a lane between.
+	 */
+	@Override
+	public String toString() {
+		return "Number of lanes: " + junctions.size();
+	}
+
+	// Repast projections
 	private Network<Object> network;
 	private ContinuousSpace<Object> space;
 	private Grid<Object> grid;
 	
-	private Junction northJunc;
-	private Junction eastJunc;
-	private Junction southJunc;
-	private Junction westJunc;
+	//List of Junctions it has a lane between
+	private List<Junction> junctions;
 			
+	/**
+	 * @param network
+	 * @param space
+	 * @param grid
+	 */
 	public Junction(Network<Object> network, ContinuousSpace<Object> space, 
 			Grid<Object> grid) {
 	
 		this.network = network;
 		this.space = space;
 		this.grid = grid;
+		this.junctions = new ArrayList<Junction>();
 	}
 	
-	public Junction getNorthJunc() {
-		return northJunc;
-	}
-
-	public void setNorthJunc(Junction northJunc) {
-		this.northJunc = northJunc;
-		network.addEdge(this, northJunc);
-	}
-
-	public Junction getEastJunc() {
-		return eastJunc;
-	}
-
-	public void setEastJunc(Junction eastJunc) {
-		this.eastJunc = eastJunc;
-		network.addEdge(this, eastJunc);
-	}
-
-	public Junction getSouthJunc() {
-		return southJunc;
-	}
-
-	public void setSouthJunc(Junction southJunc) {
-		this.southJunc = southJunc;
-		network.addEdge(southJunc, this);
-	}
-
-	public Junction getWestJunc() {
-		return westJunc;
-	}
-
-	public void setWestJunc(Junction westJunc) {
-		this.westJunc = westJunc;
-		network.addEdge(westJunc, this);
+	/**
+	 * @return List of junctions that this junction has a lane between.
+	 */
+	public List<Junction> getJunctions() {
+		return junctions;
 	}
 	
+	/**
+	 * Create a lane to another Junction. The given Junction 
+	 * is added to the List of Junctions that it now has a lane between. 
+	 * This new lane represents a new Edge on the Graph and is therefore
+	 * updated on the Network object.
+	 * 
+	 * @param junc is the other Junction the lane will be between.
+	 * @param out is a boolean flag for the lane direction being outward.
+	 */
+	public void addLane(Junction junc, boolean out) {
+		this.junctions.add(junc);
+		
+		if (out) {
+			network.addEdge(this, junc);
+		} else {
+			network.addEdge(junc, this);
+		}
+	}
+	
+	/**
+	 * Remove a lane between another Junction. This given Junction
+	 * is removed from the List of Junctions that it has a lane between. 
+	 * It represents an Edge being removed on the Graph and is therefore 
+	 * updated on the Network Object.
+	 * 
+	 * @param junc is the other Junction the lane is between.
+	 * @param out is a boolean flag for the lane direction being outward.
+	 */
+	public void removeLane(Junction junc, boolean out) {
+		this.junctions.remove(junc);
+		
+		RepastEdge<Object> edge;
+		
+		if (out) {
+			edge = network.getEdge(this, junc);
+		} else {
+			edge = network.getEdge(junc, this);
+		}
+		
+		if (edge != null) 
+			network.removeEdge(edge);
+	}
+	
+	/**
+	 * Remove all lanes joining toward this Junction, the Graph
+	 * will therefore have no Edges to this Node.
+	 */
+	public void removeAllLanes() {
+		
+		RepastEdge<Object> edgeIn;
+		RepastEdge<Object> edgeOut;
+		
+		for (Junction junc : junctions) {
+			edgeIn = network.getEdge(this, junc);
+			edgeOut = network.getEdge(junc, this);
+			network.removeEdge(edgeIn);
+			network.removeEdge(edgeOut);
+		}
+		
+		this.junctions.clear();
+	}
+	
+	/**
+	 * Sets the Junction location in the Continuous Space object.
+	 * 
+	 * @param x is the x-coordinate.
+	 * @param y is the y-coordinate.
+	 */
 	public void setLocation(int x, int y) {
 		space.moveTo(this, x, y);
 	}
 	
+	/**
+	 * Returns the Junction location in the Continuous Space object.
+	 * 
+	 * @return the Junction location as (x,y) in an NdPoint object.
+	 */
 	public NdPoint getLocation() {
 		return space.getLocation(this);
 	}

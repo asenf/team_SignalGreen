fc9c669d67ee6984815fa051b00cb28c5af62912
waqaraaziz <waqar.aziz@kcl.ac.uk>
2015-02-18 05:42:55 +0000

    Updated with new junction approach and comments
    This context builder implementation now works with the Junction multi-lane implementation. It has also been throughly updated with comments.

--- signalGreen/src/signalGreen/SignalGreenBuilder.java
+++ signalGreen/src/signalGreen/SignalGreenBuilder.java
@@ -19,15 +19,32 @@ import repast.simphony.space.grid.GridBuilderParameters;
 import repast.simphony.space.grid.SimpleGridAdder;
 import repast.simphony.space.grid.WrapAroundBorders;
 
+/**
+ * This is custom context builder implementation which is responsible 
+ * to perform the initialization of the Traffic Simulator.
+ * 
+ * @see repast.simphony.dataLoader.ContextBuilder
+ * @author Waqar, Yoann
+ *
+ */
 public class SignalGreenBuilder implements ContextBuilder<Object> {
 
+	// Repast projections
 	private Context<Object> context;
 	private Network<Object> network;
 	private ContinuousSpace<Object> space;
 	private Grid<Object> grid;
 	
+	// List to store Junctions
 	private List<Junction> junctions;
 	
+	/**
+	 * Builds and returns a context of the traffic simulation. Building a 
+	 * this consists of filling it with the agents such as Junction and
+	 * Vehicles, and adding projections such as the Graph.
+	 * 
+	 *  @see repast.simphony.dataLoader.ContextBuilder#build(repast.simphony.context.Context)
+	 */
 	@Override
 	public Context build(Context<Object> context) {
 		
@@ -52,7 +69,7 @@ public class SignalGreenBuilder implements ContextBuilder<Object> {
 				new SimpleGridAdder<Object>(),
 				true, 50, 50));
 	
-		buildLayout(junctions, Constants.SCALE);
+		buildJunctionsAndLanes(junctions, Constants.SCALE);
 		addVehicleAtJunction(junctions.get(0), 280);
 		addVehicleAtJunction(junctions.get(1), 40);
 		addVehicleAtJunction(junctions.get(4), 200);
@@ -65,7 +82,14 @@ public class SignalGreenBuilder implements ContextBuilder<Object> {
 		return context;
 	}
 	
-	public void buildLayout(List<Junction> junctions, int scale) {
+	/**
+	 * Initialize and draw out a grid of Junction objects, with the
+	 * default lanes specified. 
+	 * 
+	 * @param junctions is the List to store all Junction objects.
+	 * @param scale is the scale for the grid to be drawn.
+	 */
+	public void buildJunctionsAndLanes(List<Junction> junctions, int scale) {
 		
 		for (int i = 1; i < 5; i++) {
 		    for (int j = 1; j < 5; j++) {
@@ -76,39 +100,43 @@ public class SignalGreenBuilder implements ContextBuilder<Object> {
 		    }
 		}
 		
-		junctions.get(0).setEastJunc(junctions.get(1));
-		junctions.get(1).setEastJunc(junctions.get(2));
-		junctions.get(2).setEastJunc(junctions.get(3));
-		junctions.get(4).setEastJunc(junctions.get(5));
-		junctions.get(5).setEastJunc(junctions.get(6));
-		junctions.get(6).setEastJunc(junctions.get(7));
-		junctions.get(8).setEastJunc(junctions.get(9));
-		junctions.get(9).setEastJunc(junctions.get(10));
-		junctions.get(10).setEastJunc(junctions.get(11));
-		junctions.get(12).setEastJunc(junctions.get(13));
-		junctions.get(13).setEastJunc(junctions.get(14));
-		junctions.get(14).setEastJunc(junctions.get(15));
-		
-		junctions.get(0).setNorthJunc(junctions.get(4));
-		junctions.get(1).setNorthJunc(junctions.get(5));
-		junctions.get(2).setNorthJunc(junctions.get(6));
-		junctions.get(3).setNorthJunc(junctions.get(7));
-		junctions.get(4).setNorthJunc(junctions.get(8));
-		junctions.get(5).setNorthJunc(junctions.get(9));
-		junctions.get(6).setNorthJunc(junctions.get(10));
-		junctions.get(7).setNorthJunc(junctions.get(11));
-		junctions.get(8).setNorthJunc(junctions.get(12));
-		junctions.get(9).setNorthJunc(junctions.get(13));
-		junctions.get(10).setNorthJunc(junctions.get(14));
-		junctions.get(11).setNorthJunc(junctions.get(15));
-		
-		
-		//TrafficLight trafficJunc = new TrafficLight(network, space, grid);
-		//context.add(trafficJunc);
-		//trafficJunc.setLocation(0, 0);
+		junctions.get(0).addLane(junctions.get(1), true);
+		junctions.get(1).addLane(junctions.get(2), true);
+		junctions.get(2).addLane(junctions.get(3), true);
+		junctions.get(4).addLane(junctions.get(5), true);
+		junctions.get(5).addLane(junctions.get(6), true);
+		junctions.get(6).addLane(junctions.get(7), true);
+		junctions.get(8).addLane(junctions.get(9), true);
+		junctions.get(9).addLane(junctions.get(10), true);
+		junctions.get(10).addLane(junctions.get(11), true);
+		junctions.get(12).addLane(junctions.get(13), true);
+		junctions.get(13).addLane(junctions.get(14), true);
+		junctions.get(14).addLane(junctions.get(15), true);
+		junctions.get(0).addLane(junctions.get(4), true);
+		junctions.get(1).addLane(junctions.get(5), true);
+		junctions.get(2).addLane(junctions.get(6), true);
+		junctions.get(3).addLane(junctions.get(7), true);
+		junctions.get(4).addLane(junctions.get(8), true);
+		junctions.get(5).addLane(junctions.get(9), true);
+		junctions.get(6).addLane(junctions.get(10), true);
+		junctions.get(7).addLane(junctions.get(11), true);
+		junctions.get(8).addLane(junctions.get(12), true);
+		junctions.get(9).addLane(junctions.get(13), true);
+		junctions.get(10).addLane(junctions.get(14), true);
+		junctions.get(11).addLane(junctions.get(15), true);
 		
 	}
 	
+	/**
+	 * Add Vehicle object to a Junction object.
+	 * 
+	 * Here the vehicle is initialized with the projections and a
+	 * maximum speed for it. It is then added to the Context and 
+	 * moved to the given Junction coordinates in the Continuous Space. 
+	 * 
+	 * @param junc is the given Junction object.
+	 * @param maxSpeed is the given Maximum Speed for the vehicle.
+	 */
 	public void addVehicleAtJunction(Junction junc, int maxSpeed) {
 		
 		Vehicle vehicle = new Vehicle(space, grid, network, maxSpeed);

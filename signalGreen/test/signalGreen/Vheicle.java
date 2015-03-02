5630b0180d89a251dabcc757ed226e03a21844d9
asaalim <ayk271981@hotmail.com>
2015-03-02 00:24:03 +0000

    Vehicle test class

---
+++ signalGreen/test/signalGreen/Vheicle.java
@@ -0,0 +1,39 @@
+package signalGreen;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Vheicle {
+
+	@Test
+	public void testInitVehicle() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testStep() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testMoveTowards() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetVelocity() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testSetCurrSpeed() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testSlowDown() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}

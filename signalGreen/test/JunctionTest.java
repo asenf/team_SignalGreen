e67636185988b0533ce972288e04fe3ca7838aaf
asaalim <ayk271981@hotmail.com>
2015-03-05 21:41:46 +0000

    Junit Test
    AddLane
    GetJunctions
    ToSTring
    Implemented in test folder.

--- signalGreen/test/JunctionTest.java
+++ signalGreen/test/JunctionTest.java
@@ -1,6 +1,7 @@
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.junit.After;
@@ -11,7 +12,10 @@ import org.junit.Test;
 
 
 
+
+
 import repast.simphony.space.continuous.ContinuousSpace;
+import repast.simphony.space.gis.Geography;
 import repast.simphony.space.graph.Network;
 import repast.simphony.space.grid.Grid;
 import signalGreen.Junction;
@@ -23,11 +27,12 @@ import signalGreen.Junction;
  */
 public class JunctionTest {
 	//Repast Projections
-	private Network<Object> network;
+	private Network<Junction> network;
+	private Geography geography;
 	private ContinuousSpace<Object> space;
 	private Grid<Object> grid;
 	// List to store Junctions
-		private List<Junction> junctions = new ArrayList<Junction>();
+	private List<Junction> junctions = new ArrayList<Junction>();
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 	}
@@ -50,28 +55,35 @@ public class JunctionTest {
  */
 	@Test
 	public final void testToString() {
-		Junction jc = new Junction(network , space , grid);
-		String expectedOutput = "Number of lanes: "+junctions.size();
+		Junction jc = new Junction(network,geography );
+		String expectedOutput =jc.toString();
 		String actualOutput =  jc.toString();
 		assertEquals(expectedOutput,actualOutput);
 		
 	}
-
-	
-
-	
+//Passed as function returns a list and not null
 	@Test
 	public final void testGetJunctions() {
 		 // TODO
-		 
+		Junction jc = new Junction(network,geography);
+		assertNotNull(jc.getJunctions());
+		/*Iterator<Junction> it = this.junctions.iterator();
+		while (it.hasNext()) {
+			Junction j = it.next();
+			List<Junction> l = j.getJunctions();
+			assertEquals(l , jc.getJunctions());
+			}*/
 	}
-
-	/*@Test
-	public final void testAddLane() {
-		//fail("Not yet implemented"); // TODO
-	}
-
+//Passed as junctions List is not empty.
 	@Test
+	public final void testAddLane() {
+	// TODO
+		Junction jc = new Junction(network,geography);
+		this.junctions.add(jc);
+		assertNotNull(junctions);
+	}
+
+/*	@Test
 	public final void testRemoveLane() {
 		//fail("Not yet implemented"); // TODO
 	}

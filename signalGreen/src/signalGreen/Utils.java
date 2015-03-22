bf8083a6c48c2720a2e1c18ad98d6ac8a7737503
waqaraaziz <waqar.aziz@kcl.ac.uk>
2015-03-22 21:41:27 +0000

    Made function static
    getPopularJunction()

--- signalGreen/src/signalGreen/Utils.java
+++ signalGreen/src/signalGreen/Utils.java
@@ -54,7 +54,7 @@ public class Utils {
 	 * @param List of junctions
 	 * @return Popular junction
 	 */
-	public Junction getPopularJunction(List<Junction> junctions)
+	public static Junction getPopularJunction(List<Junction> junctions)
 	{
 		if (junctions == null) return null;
 		
